15b0c9265c38dd9d25c915942ed05917b82f9130
Junjie Jiang <jj030@bucknell.edu>
2018-10-14 19:39:21 -0400

    lint

--- output/Schedule.py
+++ output/Schedule.py
@@ -9,34 +9,38 @@ class Schedule:
 
     def __repr__(self):
         colStr = '{:<10}'.format('Item #') + '{:<20}'.format('Label')\
-        + '{:<15}'.format('Product') + '{:<15}'.format('Pack Size')\
-        + '{:<7}'.format('Cases') + '{:<10}'.format('Ross #')\
-        + '{:<10}'.format('Batches') + '{:<15}'.format('Allergens')\
-        + '{:<15}'.format('Non-K')
+            + '{:<15}'.format('Product') + '{:<15}'.format('Pack Size')\
+            + '{:<7}'.format('Cases') + '{:<10}'.format('Ross #')\
+            + '{:<10}'.format('Batches') + '{:<15}'.format('Allergens')\
+            + '{:<15}'.format('Non-K')
         schedule = '{:^114}'.format("Production Schedule") + '\n'\
-        + "Date: {self.date} \n".format(self=self)
+            + "Date: {self.date} \n".format(self=self)
         schedule += '\n' + '{:^114}'.format("Tub Line") + '\n' + colStr + '\n'
         for item in self.tub:
-            schedule += str(item) 
-        schedule += '\n' + '{:^114}'.format("Pail/Drum/Pouch Line") + '\n' + colStr + '\n'
+            schedule += str(item)
+        schedule += '\n' + '{:^114}'.format("Pail/Drum/Pouch Line") + '\n'\
+            + colStr + '\n'
         for item in self.pail:
             schedule += str(item)
-        schedule += '\n' + '{:^114}'.format("Gallon Line") + '\n' + colStr + '\n'
+        schedule += '\n' + '{:^114}'.format("Gallon Line") + '\n'\
+            + colStr + '\n'
         for item in self.gallon:
             schedule += str(item)
-        schedule += '\n' + '{:^114}'.format("Retail Line") + '\n' + colStr + '\n'
+        schedule += '\n' + '{:^114}'.format("Retail Line") + '\n'\
+            + colStr + '\n'
         for item in self.retail:
             schedule += str(item)
         return schedule
 
+
 class ScheduleItem:
     ''' an item in the schedule '''
-    def __init__(self,infoList=None):
-        self.duration = 0 # time for the item to run
-        self.allergens = 'na' # whether it is allergen
-        self.kosher = 'null' # whether it is kosher or not. Default as kosher
-        self.changeoverTime = 0 # default as 0
-        self.itemNum = '009057' 
+    def __init__(self, infoList=None):
+        self.duration = 0  # time for the item to run
+        self.allergens = 'na'  # whether it is allergen
+        self.kosher = 'null'  # whether it is kosher or not. Default as kosher
+        self.changeoverTime = 0  # default as 0
+        self.itemNum = '009057'
         self.label = 'cobbmkt originals'
         self.product = 'apple'
         self.packSize = '1/1oz'
@@ -45,10 +49,11 @@ class ScheduleItem:
         self.batches = '1'
 
     def __repr__(self):
-        return ('{self.itemNum:<10}{self.label:<20}{self.product:<15}'+
-                '{self.packSize:<15}{self.cases:<7}{self.rossNum:<10}'+
-                '{self.batches:<10}{self.allergens:<15}{self.kosher:<15}'
-                + '\n').format(self=self)
+        return ('{self.itemNum:<10}{self.label:<20}{self.product:<15}' +
+                '{self.packSize:<15}{self.cases:<7}{self.rossNum:<10}' +
+                '{self.batches:<10}{self.allergens:<15}{self.kosher:<15}' +
+                '\n').format(self=self)
+
 
 def test():
     a = Schedule()
@@ -56,3 +61,5 @@ def test():
     a.tub.append(ScheduleItem())
     a.pail.append(ScheduleItem())
     print(a)
+
+test()
