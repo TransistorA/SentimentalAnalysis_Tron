863b312fe3c43e3308c5c2cc97ca6434090517b9
Ryan Pasculano <rep015@bucknell.edu>
2018-11-09 15:30:45 -0500

    added NONE to Allergen enum

--- output/Allergen.py
+++ output/Allergen.py
@@ -1,10 +1,12 @@
 from enum import IntFlag
 class Allergen(IntFlag):
+    NONE = 0
     EGG = 1
-    BISUFATE = 2
+    BISULFITE = 2
     MUSTARD = 4
     MILK = 8
     FISH = 16
     SOY = 32
     TREENUT = 64
     WHEAT = 128
+
