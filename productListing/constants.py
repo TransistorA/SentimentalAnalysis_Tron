afa1af83700f48feb8048dc6208771f17649599e
Ryan Pasculano <rep015@bucknell.edu>
2018-11-10 11:43:42 -0500

    added constants.py back in

---
+++ productListing/constants.py
@@ -0,0 +1,26 @@
+'''
+
+Consants to denote the indecies of information in the product item listing
+
+This will be expanded at the product listing is expanded
+
+'''
+DESCRIPTION 	= 0
+PACK_SIZE 		= 1
+LABEL 			= 2
+CUSTOMER_NAME 	= 2
+ROSS_WIP 		= 3
+ALLERGEN 		= 4
+NON_K 			= 5
+COMMENTS 		= 6
+# Skip column 7 because it could introduce weird errors
+ALLERGEN_VALUE 	= 8		# Still need to implement
+						# this column gets made n
+LINE			= 9
+CPB 			= 10
+TIME_TO_RUN_BATCH = 11
+
+#FOR Finished Product Listing
+FPL_LINE = 5
+BATCH_WEIGHT = 9
+
