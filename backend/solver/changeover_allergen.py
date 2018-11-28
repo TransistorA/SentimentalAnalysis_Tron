b5e17a26c55f6edae265710f935d1cd5e8342cd1
Yash Mittal <yashmittal2009@gmail.com>
2018-11-27 22:50:54 -0500

    Add tests for changeover constraints

--- backend/solver/changeover_allergen.py
+++ backend/solver/changeover_allergen.py
@@ -9,8 +9,8 @@ TOTAL_TIME = 7 * 24 * 60  # number of minutes in a week to make it large enough
 
 class ChangeoverAllergenModel(DeadlineOverlappingModel):
     """IP model with the following constraints
-        - Normal Changeover time
-        - Changeover time for Allergens
+        - Normal changeover time
+        - Changeover time for allergens, kosher
     """
 
     def __init__(self, data):
@@ -33,3 +33,28 @@ class ChangeoverAllergenModel(DeadlineOverlappingModel):
             self.model.Ts.display()
             self.model.P.display()
         return results
+
+    def isValidSchedule(self, results):
+        """Checks if the results correspond to a valid or feasible
+        schedule
+        :param results  solved model results
+        :return [boolean] True if schedule is valid, False otherwise
+        """
+        result = DeadlineOverlappingModel.isValidSchedule(self, results)
+        if not result:
+            return False
+
+        startTimes = []
+        for i in self.model.Range:
+            startTimes.append((self.model.Ts[i].value, i))
+
+        startTimes.sort()
+        for i in range(self.num_batches - 1):
+            currBatchIdx = startTimes[i][1]
+            nextBatchIdx = startTimes[i + 1][1]
+            # ensure two consecutive batches are separated by their
+            # corresponding changeover time
+            if startTimes[i][0] + self.C_time(currBatchIdx, nextBatchIdx) > startTimes[i + 1][0]:
+                return False
+
+        return True
