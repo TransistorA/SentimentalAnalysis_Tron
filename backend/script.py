40da12ae9fdd7bb814fd1da6788966bafa96ff63
Yash Mittal <yashmittal2009@gmail.com>
2018-11-28 21:41:59 -0500

    Add a barebore integration script

---
+++ backend/script.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+# Use this script to generate a production line schedule
+# given cases needed and product listing files
+
+from casesNeeded.casesNeeded import CasesNeeded
+from productListing.productListing import ProductListing
+from solver.shifts import ShiftsModel
+from src.schedule import Schedule
+
+
+def createInputsDict(cnObj, plObj):
+    return {}
+
+
+def convertResultsToSchedule(results):
+    schObj = Schedule(date='TO FILL')
+    return schObj
+
+
+def main():
+    casesNeededFilename = ''
+    productListingFilename = ''
+
+    cnObj = CasesNeeded()
+    cnObj.readFile(casesNeededFilename)
+
+    plObj = ProductListing()
+    plObj.readFile(productListingFilename)
+
+    inputs = createInputsDict(cnObj, plObj)
+
+    m = ShiftsModel(data=inputs)
+    results = m.solve()
+    isValid = m.isValidSchedule(results)
+    if not isValid:
+        print('Generated schedule is infeasible')
+        return
+
+    scheduleObj = convertResultsToSchedule(results)
+    print(scheduleObj)
+
+
+if __name__ == '__main__':
+    main()
