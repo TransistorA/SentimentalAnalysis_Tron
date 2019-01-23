7152f1c751917ae3e83722ce6e72009793182367
Yash Mittal <yashmittal2009@gmail.com>
2019-01-23 15:16:16 -0500

    Add schedule api endpoint #33

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,32 @@
 #!/usr/bin/env python
 
-from flask import Flask
+import os
+
+from flask import Flask, request, redirect
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'uploads'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 
 @app.route('/')
 def hello():
     return 'Hello, world!'
+
+
+@app.route('/api/schedule', methods=['GET', 'POST'])
+def schedule():
+    if request.method == 'POST':
+        if 'orders' not in request.files:
+            return 'File not found', 400
+
+        orders = request.files['orders']
+        if orders.filename and orders:
+            filename = secure_filename(orders.filename)
+            orders.save(os.path.join(UPLOAD_FOLDER, filename))
+        else:
+            return 'Inappropriate file', 400
+
+    return 'File uploaded successfully'
