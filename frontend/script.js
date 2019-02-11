0223338f559af314c2eed8e78c1bad6ade326b23
StarBurnStream <1106640847@qq.com>
2019-02-11 16:17:05 -0500

    frontend v1.3 begin

--- frontend/script.js
+++ frontend/script.js
@@ -4,24 +4,37 @@ window.onload=function(){
     form.addEventListener("submit", callback, false);
   }
 
+  function handleErrors(response) {
+      if (!response.ok) {
+          throw Error(response.statusText);
+      }
+      return response;
+  }
+
   function callback (e) {
     //console.log("callback success!")
     e.preventDefault();
     var url = "http://127.0.0.1:8080/api/schedule"
     var formdata = new FormData(form);
-    document.getElementById("statusinfo").innerHTML="Files are submitted. Waiting for a schedule!"
+    document.getElementById("statusinfo").innerHTML += "Files are submitted. Waiting for a schedule!<br>"
     fetch(url,{
       method: "POST",
       body: formdata
     }).
+    then(handleErrors).
     then(result => {
       console.log(result)
       res = result.json()
+      document.getElementById("statusinfo").innerHTML+= "Response received. Fetch succeeded. <br>"
       console.log(res)
       return res
     }).
     then(result => {
       console.log(result)
+    }).
+    catch(error => {
+      console.log(error)
+      document.getElementById("statusinfo").innerHTML+= error + "<br>"
     })
 
   }
